40f5829cadc39114f3c9eeadb6248403ca322bcb
dhikshith12 <dhikshithreddy12@gmail.com>
2023-03-31 22:14:19 +0530

    structs

--- src/main.rs
+++ src/main.rs
@@ -1,60 +1,65 @@
-use std::io;
+fn main() {
+    let mut a = vec![10, 20, 30, 40, 50];
 
-fn test(_tt: i32) {
-    // take two integer inputs from user
-    let mut input = String::new();
+    // increase all elements by 1 using map
+    a = a.iter().map(|x| x + 1).collect();
 
-    io::stdin()
-        .read_line(&mut input)
-        .expect("Failed to read line");
+    let mut b = Vec::new();
 
-    let mut iter = input.split_whitespace();
-    // parse int from the received string
-    let l = iter.next().unwrap().to_string();
-    let r = iter.next().unwrap().to_string();
-    let l: i32 = l.trim().parse().expect("Please type a number!");
-    let r: i32 = r.trim().parse().expect("Please type a number!");
+    b.push(10);
+    b.push(20);
+    b.push(1);
+    b.push(8);
+    println!("{:?}", b);
 
-    if (r - l) >= 100 {
-        for i in l..r+1{
-            if i%100==90{
-                println!("{}", i);
-                break;
-            }
-        }
-    } else {
-        let mut mlh = 0;
-        let mut value = l;
-        for i in l..r + 1 {
-            let mut hi = 0;
-            let mut lo = 9;
-            let mut ic = i;
-            while ic > 0 {
-                hi = i32::max(hi, ic % 10);
-                lo = i32::min(lo, ic % 10);
-                if mlh < i32::max(mlh, hi - lo) {
-                    mlh = i32::max(mlh, hi - lo);
-                    value = i;
-                }
-                ic = ic / 10;
-            }
-        }
-        println!("{}", value);
-    }
+    b = a;
+
+    println!("{:?}", b);
+
+    let person1: Person = Person::new("Dhikshith", 21);
+
+    let mut person2: Person = Person::new("Dhikshith", 22);
+
+    person2.set_address("Bangalore");
+
+    println!("Person 1: {:?}", person1.to_string());
+    println!("Person 2: {:?}", person2.to_string());
+}    
+
+
+//Make Person implement the Debug trait
+
+struct Person{
+    name: String,
+    age: u8,
+    address: Option<String>,
 }
 
-fn main() {
-    let mut guess = String::new();
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+impl Person {
+    /// Creates a new [`Person`].
+    fn new(name: &str, age: u8) -> Person {
+        Person {
+            name: name.to_string(),
+            age,
+            address: None,
+        }
+    }
 
-    // parse int from the received string
-    let mut guess: i32 = guess.trim().parse().expect("Please type a number!");
+    fn set_address(&mut self, address: &str) {
+        self.address = Some(address.to_string());
+    }
 
-    while guess > 0 {
-        test(guess);
-        guess = guess - 1;
+
+    fn to_string(self) -> String {
+        // Stringify this object
+        let mut s = String::new();
+        s.push_str("Name: ");
+        s.push_str(&self.name);
+        s.push_str("    Age: ");
+        s.push_str(&self.age.to_string());
+        s.push_str("    Address: ");
+        s.push_str(&self.address.unwrap_or("None".to_string()));
+        s 
     }
 }
