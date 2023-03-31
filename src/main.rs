572ebe41b73ee9554daae54b5259fb5a19c0b27a
dhikshith12 <dhikshithreddy12@gmail.com>
2023-03-31 21:45:52 +0530

    modulo trick

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,60 @@
 use std::io;
 
-fn main() {
-    println!("Guess the number!");
+fn test(_tt: i32) {
+    // take two integer inputs from user
+    let mut input = String::new();
 
-    println!("Please input your guess.");
-
-    /*
-    In rust variables are immutable by default
-    */
-    let _apples = 5;
-    let mut bananas = 5; // mutable 
-
-    
-
-    let mut guess = String::new();
-
-    io::stdin().read_line(&mut guess)
+    io::stdin()
+        .read_line(&mut input)
         .expect("Failed to read line");
 
-    println!("You guessed: {}", guess);
+    let mut iter = input.split_whitespace();
+    // parse int from the received string
+    let l = iter.next().unwrap().to_string();
+    let r = iter.next().unwrap().to_string();
+    let l: i32 = l.trim().parse().expect("Please type a number!");
+    let r: i32 = r.trim().parse().expect("Please type a number!");
+
+    if (r - l) >= 100 {
+        for i in l..r+1{
+            if i%100==90{
+                println!("{}", i);
+                break;
+            }
+        }
+    } else {
+        let mut mlh = 0;
+        let mut value = l;
+        for i in l..r + 1 {
+            let mut hi = 0;
+            let mut lo = 9;
+            let mut ic = i;
+            while ic > 0 {
+                hi = i32::max(hi, ic % 10);
+                lo = i32::min(lo, ic % 10);
+                if mlh < i32::max(mlh, hi - lo) {
+                    mlh = i32::max(mlh, hi - lo);
+                    value = i;
+                }
+                ic = ic / 10;
+            }
+        }
+        println!("{}", value);
+    }
+}
+
+fn main() {
+    let mut guess = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+
+    // parse int from the received string
+    let mut guess: i32 = guess.trim().parse().expect("Please type a number!");
+
+    while guess > 0 {
+        test(guess);
+        guess = guess - 1;
+    }
 }
